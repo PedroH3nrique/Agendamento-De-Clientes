b7c26b978acaf9de1148866ffb4c5b476c124a98
PedroH3nrique <pedrohenriquerod.santos@gmail.com>
2024-12-12 15:19:35 -0300

    colocando icone nas paginas

--- admin_page.php
+++ admin_page.php
@@ -22,6 +22,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/icone_admin.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/admin.css">
     <title>Página do Administrador</title>
 </head>
